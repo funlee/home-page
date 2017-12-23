f5fa37ed82e14416fd7a2185148584fc2b2d70bc
funlee <i@funlee.cn>
2017-12-23 12:48:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+home-page
+==============
+funlee's self home-page
+
+use
+---
+```bash
+git clone https://github.com/funlee/home-page.git
+cd home-page
+npm install
+npm run start
+```
---
+++ src/app.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: funlee 
+ * @Email: i@funlee.cn 
+ * @Date: 2017-12-22 11:20:42 
+ * @Last Modified time:   2017-12-22 11:20:42 
+ * @Description: init-entry 
+ */
+import $ from 'jquery'
+import './js/iconfont'
+// import helloHbs from './hbs/hello.hbs'
+import './style/common.less'
+import './style/app.less'
+
+
+// $('.app').append(helloHbs())
